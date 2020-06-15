3291a00c8b3e3285e31280fe6be6b1f43bc08927
NiceAndFlowy <honestdetermination@gmail.com>
2020-06-15 15:28:56 -0400

    Move components into /components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { StepComponent } from './components/step/step.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
+import { StepFormComponent } from './components/step-form/step-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    StepComponent,
+    RecipeFormComponent,
+    StepFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
