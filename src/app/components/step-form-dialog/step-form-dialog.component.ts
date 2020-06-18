100f8dd89c9aa3238db016f6db62aa9d73869a1e
NiceAndFlowy <honestdetermination@gmail.com>
2020-06-18 17:38:53 -0400

    Add media url validation

--- src/app/components/step-form-dialog/step-form-dialog.component.ts
+++ src/app/components/step-form-dialog/step-form-dialog.component.ts
@@ -19,13 +19,13 @@ export class StepFormDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
-
-
   ngOnInit(): void {
+    //valid link reg expression
+    const URL_REGEXP = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
     this.form = this.formBuilder.group({
       title: ['', [Validators.required, Validators.minLength(1)]],
       description: '',
-      media: ''
+      media: ['', [Validators.pattern(URL_REGEXP)]]
     })
     console.log(this.form, this.form.valid)
   }
@@ -40,6 +40,10 @@ export class StepFormDialogComponent implements OnInit {
 
   get title() {
     return this.form.get('title');
+  }
+
+  get media() {
+    return this.form.get('media');
   }
 
   getErrorMessage() {
