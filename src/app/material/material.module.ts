2fd126f71e042e7cad5eb05bdf33def980683445
NiceAndFlowy <honestdetermination@gmail.com>
2020-06-18 17:36:45 -0400

    Add angular material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatListModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+  ],
+  exports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatListModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+  ]
+})
+export class MaterialModule { }
