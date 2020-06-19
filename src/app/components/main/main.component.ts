f072199840c9d2a01211e55cf904069544807240
NiceAndFlowy <honestdetermination@gmail.com>
2020-06-18 21:26:47 -0400

    Add styling and responsiveness

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { Recipe } from 'src/app/models/recipe';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-main',
@@ -8,8 +9,22 @@ import { Recipe } from 'src/app/models/recipe';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  constructor(private recipeService: RecipeService) { }
+  numGridCols: number = 3;
+  constructor(private recipeService: RecipeService, private breakpointObserver: BreakpointObserver) {
+    const isAtLeastMedium: string = '(min-width: 1024px)';
+    breakpointObserver.observe([Breakpoints.Small, Breakpoints.XSmall, Breakpoints.Medium, isAtLeastMedium]).subscribe(result => {
+      console.log("Breakpoints", result, this.numGridCols);
+      if (result.breakpoints[Breakpoints.XSmall]) {
+        this.numGridCols = 1;
+      }
+      else if (result.breakpoints[Breakpoints.Small]) {
+        this.numGridCols = 2;
+      }
+      else {
+        this.numGridCols = 3;
+      }
+    })
+  }
 
   get Recipes(): Recipe[] {
     console.log("Main_Recipes():", this.recipeService.Recipes);
